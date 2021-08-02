5e8d013ae1e120d03d70ce1c1d6250d34d953f67
ShinRai <shinrai1030@gmail.com>
2021-08-02 23:02:33 +0530

    Added compression and empty auth module

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
+
+import * as compression from 'compression';
+
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'verbose'],
   });
+  app.use(compression());
   const config = app.get(ConfigService);
   console.log(config);
   await app.listen(config.get('PORT'));
