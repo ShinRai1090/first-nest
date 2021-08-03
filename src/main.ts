c86e7b59a6ad14eecd42f48f13347d004ad54bc6
ShinRai <shinrai1030@gmail.com>
2021-08-03 10:30:12 +0530

    Added class-validator and class-transformer for Validations. csurf, helmet, compression for APP and express-session and its @types for session management. Also added @types/multer for file upload

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 
 import * as compression from 'compression';
+import * as helmet from 'helmet';
+import * as csurf from 'csurf';
 
 import { AppModule } from './app.module';
 
@@ -9,7 +11,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule, {
     logger: ['log', 'error', 'warn', 'verbose'],
   });
+
+  app.use(csurf());
   app.use(compression());
+  app.use(helmet());
+
   const config = app.get(ConfigService);
   console.log(config);
   await app.listen(config.get('PORT'));
